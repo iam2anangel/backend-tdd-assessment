43c843bfbdefd4198bd132349e3b77d57b9e23d7
Jennifer Browning <browningjen@yahoo.com>
2019-03-04 11:56:01 -0500

    setup args parse still need to set up echo

--- echo.py
+++ echo.py
@@ -2,21 +2,43 @@
 # -*- coding: utf-8 -*-
 """An enhanced version of the 'echo' cmd line utility"""
 
-__author__ = "???"
+__author__ = "iam2angel"  # Jen Browning
 
 
 import sys
+import argparse
 
 
 def create_parser():
-    """Creates and returns an argparse cmd line option parser"""
-    pass
+    parser = argparse.ArgumentParser(
+        description='Perform transformation on input text')
+    parser.add_argument(
+        '-u', '--upper', help='convert text to uppercase', action='store_true')
+    parser.add_argument(
+        '-l', '--lower', help='convert text to lowercase', action='store_true')
+    parser.add_argument(
+        '-t', '--title', help='convert text to titlecase', action='store_true')
+    parser.add_argument('text', help='text to be manipulated')
+    return parser
 
 
 def main(args):
-    """Implementation of echo"""
-    pass
+    parser = create_parser
+
+    if not args:
+        return open('./USAGE', 'r').read()
+
+        namespace = parser.parse_args(args)
+    text = namespace.text
+    if namespace.upper:
+        text = text.upper()
+    if namespace.lower:
+        text = text.lower()
+    if namespace.title:
+        text = text.title()
+    return text
 
 
 if __name__ == '__main__':
-    pass
+    result = main(sys.args[1:])
+    print result
